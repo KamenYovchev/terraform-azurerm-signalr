4e60f992cf6eab089f555115a79798e6aa129588
Kamen Yovchev <kamen.yovchev@wize-soluions.com>
2021-03-08 15:05:25 +0200

    add connection string as a putput

--- output.tf
+++ output.tf
@@ -6,3 +6,6 @@
 output "signalr_name" {
     value = azurerm_signalr_service.signalr.name
 }
+output "signalR_connection_string" {
+    value = azurerm_signalr_service.signalr.primary_connection_string
+}
